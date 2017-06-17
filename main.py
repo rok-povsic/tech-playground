e5727ce6562e861ccf4e9451084b594db2de26a8
Rok Povšič <rok.povsic@gmail.com>
2017-06-17 10:44:14 +0200

    Using JQuery to get graphQL data.

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ from data.starwars.schem import starwars_schema
 app = Flask(__name__)
 
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
+@app.route('/')
 def hello_world(name=None):
-    return render_template("hello.html", name=str(starwars_schema))
+    return render_template("main.html", name=name)
 
 
 app.add_url_rule(
